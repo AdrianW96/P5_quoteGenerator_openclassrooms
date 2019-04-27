d31b54f83f8277c35b4ec707c1748262e8a3af63
Adrian Wohlfarter <adrianwohlfarter@gmail.com>
2019-04-27 19:33:36 +0200

    improved code readability

--- js/main.js
+++ js/main.js
@@ -1,49 +1,11 @@
 // motivational sentence parts
-const startMot = [
-  'Think about being most of the time',
-  'Try to be',
-  'Take a step back and be',
-  'Without fear you will be',
-  'Think of yourself as a person who is',
-  'I am sure that other people think you are'
-];
-const midMot = [
-  'on time',
-  'curious about life',
-  'openminded',
-  'satisfied with what you have',
-  'enthusiastic',
-  'calm and relaxed',
-  'beautiful',
-  'attractive',
-  'a fish',
-  'reliable',
-  'trustworthy',
-  'grateful',
-  'entertaining'
-];
-const endMot = [
-  'because it is a good virtue.',
-  'that can make things awesome.',
-  'but not too much because that can hurt.',
-  'that has the power to change your life.',
-  "wouldn't that be awesome?",
-  'everybody loves that!',
-  'whenever you can!'
-];
+const startMotArr = ['Think about being most of the time', 'Try to be', 'Take a step back and be', 'Without fear you will be', 'Think of yourself as a person who is', 'I am sure that other people think you are'];
+const midMotArr = ['on time', 'curious about life', 'openminded', 'satisfied with what you have', 'enthusiastic', 'calm and relaxed', 'beautiful', 'attractive', 'a fish', 'reliable', 'trustworthy', 'grateful', 'entertaining'];
+const endMotArr = ['because it is a good virtue.', 'that can make things awesome.', 'but not too much because that can hurt.', 'that has the power to change your life.', "wouldn't that be awesome?", 'everybody loves that!', 'whenever you can!'];
 
 //fact sentence parts
-const startFct = [
-  'Drinking pineapple juice',
-  'Eating more vegetables',
-  'Solving the rubiks cube 10 times a day',
-  'Barking like a dog',
-  'Going to bed before midnight',
-  'Swimming in the sea',
-  'Cuddling with somebody or something',
-  'Imagining to be able to fly'
-];
-const midFct = [
+const startFctArr = ['Drinking pineapple juice', 'Eating more vegetables', 'Solving the rubiks cube 10 times a day', 'Barking like a dog', 'Going to bed before midnight', 'Swimming in the sea', 'Cuddling with somebody or something', 'Imagining to be able to fly'];
+const midFctArr = [
   'will make your legs longer',
   'gives you a lasting boost of happiness',
   'makes you fit and strong',
@@ -53,7 +15,7 @@ const midFct = [
   'will lead to a more natural looking skin',
   'will lead you to financial success'
 ];
-const endFct = [
+const endFctArr = [
   'so you will grow in size.',
   'so you will live a more fulfilled life.',
   'so you will never have to do the dishes again.',
@@ -67,77 +29,74 @@ const endFct = [
 let howMany = 1;
 
 // get div which holds the generated quotes
-const list = document.querySelector('.quoteOutput');
+const UIoutput = document.querySelector('.quoteOutput');
 
 //event delegation for the toggle buttons
 document.body.addEventListener('click', onNumClick);
 
+// gets generate-quotes buttons and adds event listeners
+document.querySelector('#factBtn').addEventListener('click', generateClick);
+document.querySelector('#motBtn').addEventListener('click', generateClick);
+
+document.querySelector('.clearBtn').firstElementChild.addEventListener('click', function() {
+  clear();
+});
+
+// event handler to find out how many quotes the user wants
 function onNumClick(e) {
-  if (e.target.classList.contains('eventDel')) {
+  if (e.target.classList.contains('howManyBtn')) {
     howMany = e.target.innerText;
     howMany = parseInt(howMany);
   }
 }
 
-// generate random motivational quotes function
-function generateMot() {
-  let numMotStart = Math.floor(Math.random() * startMot.length);
-  let numMotMid = Math.floor(Math.random() * midMot.length);
-  let numMotEnd = Math.floor(Math.random() * endMot.length);
-
-  let motOutput =
-    startMot[numMotStart] + ' ' + midMot[numMotMid] + ', ' + endMot[numMotEnd];
-  return motOutput;
-}
-
-// generate random facts function
-function generateFct() {
-  let numFctStart = Math.floor(Math.random() * startFct.length);
-  let numFctMid = Math.floor(Math.random() * midFct.length);
-  let numFctEnd = Math.floor(Math.random() * endFct.length);
-
-  let fctOutput =
-    startFct[numFctStart] + ' ' + midFct[numFctMid] + ', ' + endFct[numFctEnd];
-  return fctOutput;
-}
-
-// gets generate-quotes buttons and adds event listeners
-document.querySelector('#factBtn').addEventListener('click', onClick);
-document.querySelector('#motBtn').addEventListener('click', onClick);
-
-// generateQuotesLoop
-function generate(clickedBtn) {
-  for (i = 0; i < howMany; i++) {
-    let curQuote;
-    if (clickedBtn === 'factBtn') {
-      curQuote = generateFct();
-    } else if (clickedBtn === 'motBtn') {
-      curQuote = generateMot();
-    }
-    const quoteElmt = document.createElement('p');
-    quoteElmt.className = 'rounded p-3 shadow-sm';
-    quoteElmt.innerText = curQuote;
-    list.appendChild(quoteElmt);
-  }
-}
-
 // handler to generate quotes
-function onClick(e) {
+function generateClick(e) {
   clear();
   // find out which (fact or motivational) button was clicked
   let btnClicked = e.target.id;
   generate(btnClicked);
 }
 
-document
-  .querySelector('.clearBtn')
-  .firstElementChild.addEventListener('click', function() {
-    clear();
-  });
+// generate quotes loop
+function generate(btnClicked) {
+  for (i = 0; i < howMany; i++) {
+    let curQuote;
+    if (btnClicked === 'factBtn') {
+      curQuote = generateFct();
+    } else if (btnClicked === 'motBtn') {
+      curQuote = generateMot();
+    }
+    const quoteElmt = document.createElement('p');
+    quoteElmt.className = 'rounded p-3 shadow-sm';
+    quoteElmt.innerText = curQuote;
+    UIoutput.appendChild(quoteElmt);
+  }
+}
+
+// generate random motivational quotes function
+function generateMot() {
+  let motStart = startMotArr[Math.floor(Math.random() * startMotArr.length)];
+  let motMid = midMotArr[Math.floor(Math.random() * midMotArr.length)];
+  let motEnd = endMotArr[Math.floor(Math.random() * endMotArr.length)];
+
+  let motOutput = motStart + ' ' + motMid + ', ' + motEnd;
+  return motOutput;
+}
+
+// generate random facts function
+function generateFct() {
+  let fctStart = startFctArr[Math.floor(Math.random() * startFctArr.length)];
+  let fctMid = midFctArr[Math.floor(Math.random() * midFctArr.length)];
+  let fctEnd = endFctArr[Math.floor(Math.random() * endFctArr.length)];
+
+  let fctOutput = fctStart + ' ' + fctMid + ', ' + fctEnd;
+  return fctOutput;
+}
 
 // Delete previous quotes before outputting new ones
 function clear() {
-  while (list.firstChild) {
-    list.removeChild(list.firstChild);
+  while (UIoutput.firstChild) {
+    UIoutput.removeChild(UIoutput.firstChild);
   }
 }
